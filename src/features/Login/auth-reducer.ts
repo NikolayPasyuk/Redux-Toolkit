9dae7ed1990c01b05ff03c2180e6f7de2f7024dd
NikolayPasyuk <kolyan-1812@mail.ru>
2023-03-02 19:59:51 +0300

    create a reducer

--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -16,21 +16,21 @@ const initialState = {
 const slice = createSlice({
     name: 'auth',
     initialState: initialState,
-    reducers: {
-
-    }
+    reducers: {}
 })
 
 type InitialStateType = typeof initialState
 
-export const authReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
-    switch (action.type) {
-        case 'login/SET-IS-LOGGED-IN':
-            return {...state, isLoggedIn: action.value}
-        default:
-            return state
-    }
-}
+export const authReducer = slice.reducer
+
+//     (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+//     switch (action.type) {
+//         case 'login/SET-IS-LOGGED-IN':
+//             return {...state, isLoggedIn: action.value}
+//         default:
+//             return state
+//     }
+// }
 // actions
 export const setIsLoggedInAC = (value: boolean) =>
     ({type: 'login/SET-IS-LOGGED-IN', value} as const)
