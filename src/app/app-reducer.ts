c0e7297ebcfc5a8c29768aa42494ea6c7c73be48
NikolayPasyuk <kolyan-1812@mail.ru>
2023-03-02 20:36:10 +0300

    create a reducer

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -14,23 +14,22 @@ const initialState: InitialStateType = {
 const slice = createSlice({
     name: 'app',
     initialState: initialState,
-    reducers: {
-
-    }
+    reducers: {}
 })
 
-export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
-    switch (action.type) {
-        case 'APP/SET-STATUS':
-            return {...state, status: action.status}
-        case 'APP/SET-ERROR':
-            return {...state, error: action.error}
-        case 'APP/SET-IS-INITIALIZED':
-            return {...state, isInitialized: action.isInitialized}
-        default:
-            return {...state}
-    }
-}
+export const appReducer = slice.reducer
+//     (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+//     switch (action.type) {
+//         case 'APP/SET-STATUS':
+//             return {...state, status: action.status}
+//         case 'APP/SET-ERROR':
+//             return {...state, error: action.error}
+//         case 'APP/SET-IS-INITIALIZED':
+//             return {...state, isInitialized: action.isInitialized}
+//         default:
+//             return {...state}
+//     }
+// }
 
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 export type InitialStateType = {
